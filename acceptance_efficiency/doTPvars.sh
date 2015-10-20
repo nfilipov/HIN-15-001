66a45015b1e9ddae4f4856e0cd5555280a567294
Emilien Chapon <emilien.chapon@cern.ch>
2015-10-20 17:11:37 +0200

    Local changes on polui

--- acceptance_efficiency/doTPvars.sh
+++ acceptance_efficiency/doTPvars.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pwd_=$PWD
+
 for i in `seq 0 100`; do
    echo $i
    sleep 1
@@ -8,7 +10,7 @@ for i in `seq 0 100`; do
    done
    mkdir results_$i
    cd results_$i
-   # nice root -l -b -q /home/llr/cms/chapon/data_CMS/upsilon/effs/doEff.C'("/home/llr/cms/chapon/data_CMS/upsilon/effs/trees/Upsi_Histos_Pyquen*2S*",2,true,2,1,'$i')' >/dev/null &
-   nice root -l -b -q /home/llr/cms/chapon/data_CMS/upsilon/effs/doEff.C'("/home/llr/cms/chapon/data_CMS/upsilon/effs/trees/Upsi_Histos_ppNoCutMC1S_fixedFSR.root",1,false,2,2,'$i')' >/dev/null &
-   cd -
+   # nice root -l -b -q /home/llr/cms/chapon/data_CMS/upsilon/effs/doEff.C'("/home/llr/cms/chapon/data_CMS/upsilon/effs/trees/Upsi_Histos_Pyquen*2S*",2,true,2,2,'$i')' 2>&1 >/dev/null &
+   nice root -l -b -q /home/llr/cms/chapon/data_CMS/upsilon/effs/doEff.C'("/home/llr/cms/chapon/data_CMS/upsilon/effs/trees/Upsi_Histos_ppNoCutMC1S_fixedFSR.root",1,false,2,2,'$i')' 2>&1 >/dev/null &
+   cd $pwd_
 done
